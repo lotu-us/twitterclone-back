e9b80c00e3c574cd567d896327954f52e4ac721e
thwjd <thwjd1356@naver.com>
2022-02-15 15:38:39 +0900

    message.properties 적용

--- src/main/java/com/example/twitterclone/dto/CommentDTO.java
+++ src/main/java/com/example/twitterclone/dto/CommentDTO.java
@@ -15,36 +15,36 @@ public class CommentDTO {
     @Data
     @NoArgsConstructor
     public static class Insert {
-        @NotBlank(message = "닉네임을 입력해주세요.")
-        @Size(max = 10, message = "10자 이하로 입력해주세요.")
+        @NotBlank(message = "{nickname.NotBlank}")
+        @Size(max = 10, message = "{nickname.SizeMax}")
         private String nickname;
 
-        @NotBlank(message = "비밀번호를 입력해주세요.")
-        @Pattern(regexp = PasswordRegexpConfig.reg, message = "비밀번호는 영소문자, 숫자, 특수문자가 1문자 이상 포함되어야하며 5자 이상 20자 이하로 설정해주세요")
+        @NotBlank(message = "{password.NotBlank}")
+        @Pattern(regexp = PasswordRegexpConfig.reg, message = "{password.Pattern}")
         private String password;
 
-        @NotBlank(message = "내용을 입력해주세요.")
-        @Size(max = 100, message = "100자 이하로 입력해주세요.")
+        @NotBlank(message = "{content.NotBlank}")
+        @Size(max = 50, message = "{content.SizeMax}")
         private String content;
     }
 
     @Data
     @NoArgsConstructor
     public static class Update{
-        @NotBlank(message = "비밀번호를 입력해주세요.")
-        @Pattern(regexp = PasswordRegexpConfig.reg, message = "비밀번호는 영소문자, 숫자, 특수문자가 1문자 이상 포함되어야하며 5자 이상 20자 이하로 설정해주세요")
+        @NotBlank(message = "{password.NotBlank}")
+        @Pattern(regexp = PasswordRegexpConfig.reg, message = "{password.Pattern}")
         private String password;
 
-        @NotBlank(message = "내용을 입력해주세요.")
-        @Size(max = 100, message = "100자 이하로 입력해주세요.")
+        @NotBlank(message = "{content.NotBlank}")
+        @Size(max = 50, message = "{content.SizeMax}")
         private String content;
     }
 
     @Data
     @NoArgsConstructor
     public static class Delete{
-        @NotBlank(message = "비밀번호를 입력해주세요.")
-        @Pattern(regexp = PasswordRegexpConfig.reg, message = "비밀번호는 영소문자, 숫자, 특수문자가 1문자 이상 포함되어야하며 5자 이상 20자 이하로 설정해주세요")
+        @NotBlank(message = "{password.NotBlank}")
+        @Pattern(regexp = PasswordRegexpConfig.reg, message = "{password.Pattern}")
         private String password;
     }
 
