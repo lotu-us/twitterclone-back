3f2cdcdf7e791e139f738fb05a559973cd46d82a
thwjd <thwjd1356@naver.com>
2022-02-20 01:11:27 +0900

    cors설정 header추가

--- src/main/java/com/example/twitterclone/config/WebConfig.java
+++ src/main/java/com/example/twitterclone/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")              //cors를 적용할 URL패턴
                 .allowedOrigins("http://localhost:3000", "http://localhost:8080")   //자원 공유를 허락할 Origin
-                .allowedMethods("*");   //자원 공유를 허락할 HTTP Method
+                .allowedMethods("*")   //자원 공유를 허락할 HTTP Method
+                .allowedHeaders("*");
     }
 }
