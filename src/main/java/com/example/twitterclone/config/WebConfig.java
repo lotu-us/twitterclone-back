2c6f1bbc5ee55a9ce0ab8ceab9890ebc62f4e26f
thwjd <thwjd1356@naver.com>
2022-02-19 22:42:35 +0900

    cors설정 추가

---
+++ src/main/java/com/example/twitterclone/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.twitterclone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")              //cors를 적용할 URL패턴
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")   //자원 공유를 허락할 Origin
+                .allowedMethods("*");   //자원 공유를 허락할 HTTP Method
+    }
+}
