becfae78da9eef097ce62e039cae814df8465479
thwjd <thwjd1356@naver.com>
2022-02-20 01:21:00 +0900

    cors설정 테스트

--- src/main/java/com/example/twitterclone/config/WebConfig.java
+++ src/main/java/com/example/twitterclone/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")              //cors를 적용할 URL패턴
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")   //자원 공유를 허락할 Origin
+                //.allowedOrigins("http://localhost:3000", "http://localhost:8080")   //자원 공유를 허락할 Origin
+                .allowedOrigins("*")
                 .allowedMethods("*")   //자원 공유를 허락할 HTTP Method
                 .allowedHeaders("*");
     }
